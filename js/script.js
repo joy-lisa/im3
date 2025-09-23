6acced0275365eddbfe877e70f079ef2c1a7ba31
Lisa Zimmermann <lisa.zimmermann@stud.fhgr.ch>
2025-09-23 14:34:35 +0200

    g

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
-const orte = [orte.json];
+fetch orte = [orte.json];
 
-// Dropdown und Ausgabefeld holen
-const dropdown = document.getElementById("orte");
-const ausgabe = document.getElementById("ausgabe");
-
-// JSON in Dropdown einfügen
-orte.forEach(ort => {
-  const option = document.createElement("option");
-  option.value = ort.id;
-  option.textContent = ort.name;
-  dropdown.appendChild(option);
-});
-
-// Eventlistener: Auswahl anzeigen
-dropdown.addEventListener("change", () => {
-  const selected = orte.find(o => o.id == dropdown.value);
-  ausgabe.textContent = selected ? `Du hast ${selected.name} gewählt.` : "";
-});
-
-.date {
-
+async function loadOrteDetails(url_orte) {
+    try {
+        const response = await fetch(url_orte);
+        const answer = await response.json();
+        // array all_mountains_with_details mit den details aus den detaildaten befüllen
+        return {
+            id: answer.id,
+            canton: answer.canton,
+            name: answer.name,
+            height: answer.height,
+            lat: answer.lat,
+            long: answer.long,
+            image: answer.image
+        };
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
 }
+for (const [orte] of all_orte_with_details.entries()) {
+    const details = await loadOrteDetails(orte.url);
+    all_orte_with_details.push(details);
+}
+
+// Eindeutige Orte sammeln und sortieren
+const uniqueOrte = [...new Set(orte.map(o => o.name.trim()))].sort();
+
+// Dropdown mit Orten füllen
+const selectElement = document.getElementById('orte');
+uniqueOrte.forEach(ort => {
+    const option = document.createElement('option');
+    option.value = ort;
+    option.textContent = ort;
+    selectElement.appendChild(option);
+});

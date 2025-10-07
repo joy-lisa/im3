3f82b9de0250f8cb32f0efa3934d3e2c708a241f
Lisa Zimmermann <lisa.zimmermann@stud.fhgr.ch>
2025-10-07 12:36:59 +0200

    html

--- js/script.js
+++ js/script.js
@@ -1,37 +1,58 @@
+//api url https://im3hs25.jannastutz.ch/php/unload.php
 
-// Dropdown befüllen
-async function ladeOrte() {
-  try {
-    // JSON-Datei laden
-    const response = await fetch("orte.json");
-    const orte = await response.json();
+fetch('https://im3hs25.jannastutz.ch/php/unload.php')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Fehler beim Abrufen der Daten:', error);
+  });
 
-    console.log("Geladene Rohdaten:", orte);
+  
 
-    // Dropdown-Element holen
-    const dropdown = document.getElementById("orte");
-    const ausgabe = document.getElementById("ausgabe");
 
-    // Orte einfügen
-    orte.forEach(ort => {
-      console.log("Ort-Objekt:", ort);
-      const option = document.createElement("option");
-      option.value = ort.id;        // interne ID
-      option.textContent = ort.ort; // sichtbarer Name
-      dropdown.appendChild(option);
-    });
 
-    // Wenn jemand auswählt → Text anzeigen
-   // dropdown.addEventListener("change", () => {
-  //ausgabe.textContent = dropdown.value
-   // ? `Du hast ${dropdown.value} gewählt.`
-   // : "";
-//});
-  } catch (error) {
-    console.error("Fehler beim Laden:", error);
-  }
-}
 
-// Funktion starten
-ladeOrte();
+
+
+
+
+
+
+// // Dropdown befüllen
+// async function ladeOrte() {
+//   try {
+//     // JSON-Datei laden
+//     const response = await fetch("orte.json");
+//     const orte = await response.json();
+
+//     console.log("Geladene Rohdaten:", orte);
+
+//     // Dropdown-Element holen
+//     const dropdown = document.getElementById("orte");
+//     const ausgabe = document.getElementById("ausgabe");
+
+//     // Orte einfügen
+//     orte.forEach(ort => {
+//       console.log("Ort-Objekt:", ort);
+//       const option = document.createElement("option");
+//       option.value = ort.id;        // interne ID
+//       option.textContent = ort.ort; // sichtbarer Name
+//       dropdown.appendChild(option);
+//     });
+
+//     // Wenn jemand auswählt → Text anzeigen
+//    // dropdown.addEventListener("change", () => {
+//   //ausgabe.textContent = dropdown.value
+//    // ? `Du hast ${dropdown.value} gewählt.`
+//    // : "";
+// //});
+//   } catch (error) {
+//     console.error("Fehler beim Laden:", error);
+//   }
+// }
+
+// // Funktion starten
+// ladeOrte();
 
